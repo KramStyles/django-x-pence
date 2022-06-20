7193ba7c70378cf96e8083fe56c88be33010e6f8
kramstyles <mark.eke@decagon.dev>
2022-06-20 10:55:28 +0100

    moved utils to functions

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,4 +1,10 @@
 import json, string, calendar, datetime, random, re
+import base64
+import random
+import secrets
+
+from decouple import config
+from django.core.mail import send_mail
 
 
 class Functions:
@@ -234,3 +240,43 @@ class Functions:
                 VAR = self.namestr(x, globals())
                 msg += f"<br>{VAR} is Empty"
         return msg
+
+    # Generate OTP
+    @staticmethod
+    def generate_otp():
+        otp_code = random.randrange(100000, 999999)
+        return otp_code
+
+    @staticmethod
+    def generate_randoms(num=10):
+        return secrets.token_urlsafe(num + 2)
+
+    # Send email
+    @staticmethod
+    def send_email(data):
+        email_subject = data["email_subject"]
+        message = data["email_body"]
+        email_from = config("EMAIL_HOST_USER", default="dummy@gmail.com")
+        email_to = data["to_email"]
+        html_format = data["email_body"]
+        try:
+            send_mail(
+                email_subject,
+                message,
+                email_from,
+                email_to,
+                fail_silently=False,
+                html_message=html_format,
+            )
+        except Exception as err:
+            raise err
+
+    # Encode email
+    @staticmethod
+    def encode_email(email: str or bytes):
+        return base64.b64encode(email.encode()).decode()
+
+    # Decode email
+    @staticmethod
+    def decode_email(encoded_email: str or bytes):
+        return base64.b64decode(encoded_email).decode()
